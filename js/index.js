73fe57a5e8d2b3137a7f2ae1d03aa2db92d2903b
Kuza <74424844+Kuzafkto@users.noreply.github.com>
2022-12-09 15:24:06 -0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -79,3 +79,22 @@ function showDivs(n, no) {
   }
   x[slideIndex[no] - 1].style.display = "block";
 }
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+function myFunction2() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
